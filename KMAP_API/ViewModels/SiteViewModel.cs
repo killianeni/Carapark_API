0339c4fb2481facf056c2f44a2192db94d2f1982
Morgan Potiron <morgan.potiron2017@campus-eni.fr>
2020-06-25 14:41:13 +0200

    Ajout SiteViewModel et SitesController

---
+++ KMAP_API/ViewModels/SiteViewModel.cs
@@ -0,0 +1,30 @@
+﻿using KMAP_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMAP_API.ViewModels
+{
+    public class SiteViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Libelle { get; set; }
+
+        public string NomEntreprise { get; set; }
+
+        public ICollection<Personnel> Personnels { get; set; }
+
+        public ICollection<Vehicule> Vehicules { get; set; }
+
+        public SiteViewModel(Site s)
+        {
+            Id = s.Id;
+            Libelle = s.Libelle;
+            NomEntreprise = s.Entreprise.Libelle;
+            Personnels = s.Personnels;
+            Vehicules = s.Vehicules;
+        }
+    }
+}
