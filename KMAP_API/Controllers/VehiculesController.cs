dbd3e90cda86775c8aa4f7b511868c8e2f4a8f58
Morgan Potiron <morgan.potiron2017@campus-eni.fr>
2020-06-25 14:23:25 +0200

    Ajout VehiculeViewModel et VehiculesController

---
+++ KMAP_API/Controllers/VehiculesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KMAP_API.Data;
+using KMAP_API.Models;
+using KMAP_API.ViewModels;
+
+namespace KMAP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculesController : ControllerBase
+    {
+        private readonly KmapContext _context;
+
+        public VehiculesController(KmapContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vehicules
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VehiculeViewModel>>> GetVehicule()
+        {
+            var v = new List<VehiculeViewModel>();
+            foreach (var vehicule in await _context.Vehicule.Include(v => v.Cles).ToListAsync())
+            {
+                v.Add(new VehiculeViewModel(vehicule));
+            }
+            return v;
+        }
+
+        // GET: api/Vehicules/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VehiculeViewModel>> GetVehicule(Guid id)
+        {
+            var vehicule = new VehiculeViewModel(await _context.Vehicule.Include(v => v.Cles).FirstOrDefaultAsync(v => v.Id == id));
+
+            if (vehicule == null)
+            {
+                return NotFound();
+            }
+
+            return vehicule;
+        }
+
+        // PUT: api/Vehicules/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVehicule(Guid id, Vehicule vehicule)
+        {
+            if (id != vehicule.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vehicule).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehiculeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vehicules
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Vehicule>> PostVehicule(Vehicule vehicule)
+        {
+            _context.Vehicule.Add(vehicule);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVehicule", new { id = vehicule.Id }, vehicule);
+        }
+
+        // DELETE: api/Vehicules/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Vehicule>> DeleteVehicule(Guid id)
+        {
+            var vehicule = await _context.Vehicule.FindAsync(id);
+            if (vehicule == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vehicule.Remove(vehicule);
+            await _context.SaveChangesAsync();
+
+            return vehicule;
+        }
+
+        private bool VehiculeExists(Guid id)
+        {
+            return _context.Vehicule.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ KMAP_API/ViewModels/VehiculeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using KMAP_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMAP_API.ViewModels
+{
+    public class VehiculeViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string NumImmat { get; set; }
+
+        public string Modele { get; set; }
+
+        public int NbPlaces { get; set; }
+
+        public int NbPortes { get; set; }
+
+        public string TypeCarbu { get; set; }
+
+        public bool Actif { get; set; }
+
+        public ICollection<Cle> Cles { get; set; }
+
+        public VehiculeViewModel(Vehicule v)
+        {
+            Id = v.Id;
+            NumImmat = v.NumImmat;
+            Modele = v.Modele;
+            NbPlaces = v.NbPlaces;
+            NbPortes = v.NbPortes;
+            TypeCarbu = v.TypeCarbu;
+            Actif = v.Actif;
+            Cles = v.Cles;
+        }
+    }
+}
