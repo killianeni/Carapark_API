df80110d8a4c08e16ecf737e4a22fca79ca78e75
Morgan Potiron <morgan.potiron2017@campus-eni.fr>
2020-08-26 15:46:35 +0200

    Modif UtilViewModel, Ajout RoleViewModel

---
+++ KMAP_API/ViewModels/RoleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMAP_API.ViewModels
+{
+    public class RoleViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Libelle { get; set; }
+
+        public RoleViewModel()
+        {
+
+        }
+    }
+}
--- KMAP_API/ViewModels/UtilisateurViewModel.cs
+++ KMAP_API/ViewModels/UtilisateurViewModel.cs
@@ -18,9 +18,9 @@ namespace KMAP_API.ViewModels
 
         public string Permis { get; set; }
 
-        public string NomRole { get; set; }
+        public RoleViewModel Role { get; set; }
 
-        public string NomSite { get; set; }
+        public SiteViewModel Site { get; set; }
 
         public ICollection<Personnel_Reservation> Personnel_Reservations { get; set; }
 
@@ -32,8 +32,16 @@ namespace KMAP_API.ViewModels
             Prenom = u.Prenom;
             Mail = u.Mail;
             Permis = u.Permis;
-            NomRole = u.Role.Libelle;
-            NomSite = u.Site.Libelle;
+            Role = new RoleViewModel()
+            {
+                Id = u.Role.Id,
+                Libelle = u.Role.Libelle
+            };
+            Site = new SiteViewModel()
+            {
+                Id = u.Site.Id,
+                Libelle = u.Site.Libelle
+            };
             Personnel_Reservations = u.Personnel_Reservations;
         }
     }
